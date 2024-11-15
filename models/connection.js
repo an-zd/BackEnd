ea85ae66753788c4e70bdf6b38f187be0cea7a5e
sandeep sanwle <sandeepsanwle74@gmail.com>
2024-11-15 18:51:44 +0530

    db string updated

--- models/connection.js
+++ models/connection.js
@@ -1,10 +1,9 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 dotenv.config();
-const URL = "mongodb://127.0.0.1:27017/eAuction-6aug";
-// const URL =
-//   "mongodb+srv://dwivedia988:Adarsh8349@cluster0.ufh31.mongodb.net/e-auction?retryWrites=true&w=majority&appName=Cluster0";
+// const URL = "mongodb://127.0.0.1:27017/eAuction-6aug";
+const URL = "mongodb+srv://dwivedia988:Adarsh8349@cluster0.ufh31.mongodb.net/e-auction?retryWrites=true&w=majority&appName=Cluster0";
 // const URL = process.env.NODE_APP_DATABASE_URL;
-// console.log(URL);
+console.log(URL);
 mongoose.connect(URL);
 console.log("Successfully connected to mongodb database...");
